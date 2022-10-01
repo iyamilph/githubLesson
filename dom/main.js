add1e748edce459900e8040df337bccb72ca7e02
iyamilph <114806692+iyamilph@users.noreply.github.com>
2022-10-01 16:16:45 +0600

    Add files via upload

---
+++ dom/main.js
@@ -0,0 +1,28 @@
+//const list = document.querySelector('.list');
+// const listName = document.querySelector('#listName');
+// const listTag = document.querySelector("ol");
+// const li = document.querySelectorAll(".list>li")
+
+// const listId = document.getElementById("listName")
+
+// const listClass = document.getElementsByClassName("list")
+
+// console.log(listClass);
+// const nameDima = document.getElementById("Dima")
+// const ol = nameDima.parentNode
+
+
+// // const children = ol.childNodes
+// // const childrenLi = ol.children
+
+// // const firstLi = ol.firstChild
+
+// // const nameBakir = nameDima.previousSibling
+
+// document.body.style.backgroundColor = "red"
+
+// ol.style.color = "white"
+
+// nameDima.classList.add("list-font")
+
+// nameDima.classList.remove("nameDima")
